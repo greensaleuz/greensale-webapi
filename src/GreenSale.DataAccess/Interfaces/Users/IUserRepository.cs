477cf12ae9e4505b69072150a2cb3a343195e932
rustambekovich <muhammadqodir5050@gmail.com>
2023-08-10 08:25:05 +0500

    DATAACCES : User Add GetbyPhono

--- src/GreenSale.DataAccess/Interfaces/Users/IUserRepository.cs
+++ src/GreenSale.DataAccess/Interfaces/Users/IUserRepository.cs
@@ -5,5 +5,7 @@ using GreenSale.Domain.Entites.Users;
 namespace GreenSale.DataAccess.Interfaces.Users
 {
     public interface IUserRepository : IRepository<User, UserViewModel>, ISearchable<UserViewModel>
-    {}
+    {
+        public Task<User> GetByPhoneAsync(string phone);
+    }
 }
