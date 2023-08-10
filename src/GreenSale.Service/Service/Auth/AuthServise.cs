5c062b8f73726486126c0076f0bca68e53ddb334
rustambekovich <muhammadqodir5050@gmail.com>
2023-08-10 08:55:37 +0500

    changed

--- src/GreenSale.Service/Service/Auth/AuthServise.cs
+++ src/GreenSale.Service/Service/Auth/AuthServise.cs
@@ -10,10 +10,10 @@ public class AuthServise : IAuthServices
 {
     private const int CACHED_FOR_MINUTS_REGISTER = 60;
     private const int CACHED_FOR_MINUTS_VEFICATION = 5;
+    private const int VERIFICATION_MAXIMUM_ATTEMPTS = 3;
 
     private const string REGISTER_CACHE_KEY = "register_";
     private const string VERIFY_REGISTER_CACHE_KEY = "verify_register_";
-    private const int VERIFICATION_MAXIMUM_ATTEMPTS = 3;
 
     private  readonly IMemoryCache _memoryCache;
     private readonly IUserRepository _userRepository;
@@ -23,6 +23,7 @@ public class AuthServise : IAuthServices
         this._memoryCache = memoryCache;
         this._userRepository = userRepository;
     }
+
     public async Task<(bool Result, int CachedMinutes)> RegisterAsync(UserRegisterDto dto)
     {
         var dbResult = await _userRepository.GetByPhoneAsync(dto.PhoneNumber);
@@ -39,6 +40,7 @@ public class AuthServise : IAuthServices
             _memoryCache.Set(REGISTER_CACHE_KEY + dto.PhoneNumber, dto, TimeSpan.FromMinutes
                 (CACHED_FOR_MINUTS_REGISTER));
         }
+
         return (Result: true, CachedMinutes: CACHED_FOR_MINUTS_REGISTER);
     }
 
