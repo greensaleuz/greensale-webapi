b039b23e2691a3c73961d60020e21d1b05b2a56c
SamandarYigitaliyev <programmist4467@gmail.com>
2023-08-10 10:57:20 +0500

    SERVICE:FileService (Delete func) Created

---
+++ src/GreenSale.Service/Service/Common/FileService.cs
@@ -0,0 +1,38 @@
+﻿using GreenSale.Service.Interfaces.Common;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Hosting;
+namespace GreenSale.Service.Service.Common;
+
+public class FileService : IFileService
+{
+    private readonly string MEDIA = "media";
+    private readonly string IMAGES = "image";
+    private readonly string ROOTPATH;
+
+    public FileService(IWebHostEnvironment env)
+    {
+        ROOTPATH = env.WebRootPath;
+    }
+
+    public async Task<bool> DeleteImageAsync(string subpath)
+    {
+        string path =  Path.Combine(ROOTPATH, subpath);
+
+        if(File.Exists(path))
+        {
+            await Task.Run(() =>
+            {
+                File.Delete(path);
+            });
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public Task<string> UploadImageAsync(IFormFile image)
+    {
+        throw new NotImplementedException();
+    }
+}
