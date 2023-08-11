1b7bb83b8fb148c99606691110cebd7d56399b82
rustambekovich <muhammadqodir5050@gmail.com>
2023-08-11 09:58:52 +0500

    WEP-API Architekture Controller

---
+++ src/GreenSale.WebApi/Controllers/Admin/AdminBaseController.cs
@@ -0,0 +1,10 @@
+﻿using GreenSale.WebApi.Controllers.Common;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenSale.WebApi.Controllers.Admin;
+
+[Route("api/[controller]")]
+[Authorize(Roles = "Admin")]
+public class AdminBaseController : BaseController
+{}
---
+++ src/GreenSale.WebApi/Controllers/Common/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GreenSale.WebApi.Controllers.Common
+{
+    [Route("api/[controller]")]
+    [AllowAnonymous]
+    public class BaseController : ControllerBase
+    {}
+}
