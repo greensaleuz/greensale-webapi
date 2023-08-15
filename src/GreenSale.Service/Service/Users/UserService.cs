6887807314721e080ca1550c752c3d0502206126
rustambekovich <muhammadqodir5050@gmail.com>
2023-08-15 10:28:40 +0500

    SERVICE : User Interface and User Service Created

---
+++ src/GreenSale.Service/Service/Users/UserService.cs
@@ -0,0 +1,88 @@
+﻿using GreenSale.Application.Exceptions.Users;
+using GreenSale.Application.Utils;
+using GreenSale.DataAccess.Interfaces;
+using GreenSale.DataAccess.Interfaces.Users;
+using GreenSale.DataAccess.ViewModels.Users;
+using GreenSale.Domain.Entites.Users;
+using GreenSale.Persistence.Dtos;
+using GreenSale.Service.Interfaces.Common;
+using GreenSale.Service.Interfaces.Users;
+using GreenSale.Service.Security;
+
+namespace GreenSale.Service.Service.Users;
+
+public class UserService : IUserService
+{
+    private readonly IPaginator _paginator;
+    private readonly IUserRepository _userRepository;
+
+    public UserService(
+        IUserRepository userRepository,
+        IPaginator paginator)
+    {
+        this._paginator = paginator;
+        this._userRepository = userRepository;
+    }
+
+    public async Task<long> CountAsync()
+    {
+        var DbResult = await _userRepository.CountAsync();
+
+        return DbResult;
+    }
+
+    public async Task<bool> DeleteAsync(long userId)
+    {
+        var DbFound = await _userRepository.GetByIdAsync(userId);
+        if (DbFound is null)
+            throw new UserNotFoundException();
+       
+        var DbResult = await _userRepository.DeleteAsync(userId);
+
+        return DbResult > 0;
+    }
+
+    public async Task<List<UserViewModel>> GetAllAsync(PaginationParams @params)
+    {
+        var DbResult = await _userRepository.GetAllAsync(@params);
+        var count = await _userRepository.CountAsync();
+        _paginator.Paginate(count, @params);
+
+        return DbResult;
+    }
+
+    public async Task<UserViewModel> GetByIdAsync(long userId)
+    {
+        var DbFound = await _userRepository.GetByIdAsync(userId);
+        if(DbFound is null)
+            throw new UserNotFoundException();
+        var DbResult = await _userRepository.GetByIdAsync(userId);
+
+        return DbResult;
+    }
+
+    public async Task<bool> UpdateAsync(long userId, UserRegisterDto dto)
+    {
+        var DbFound = await _userRepository.GetByIdAsync(userId);
+
+        if( DbFound is null)
+            throw new UserNotFoundException();
+
+        User user = new User()
+        {
+            FirstName = dto.FirstName,
+            LastName = dto.LastName,
+            PhoneNumber = dto.PhoneNumber,
+            PhoneNumberConfirme = true,
+            Region = dto.Region,
+            District = dto.District,
+            Address = dto.Address
+        };
+        var hasher = PasswordHasher.Hash(dto.Password);
+        user.PasswordHash = hasher.Hash;
+        user.Salt = hasher.Salt;
+        var DbResult = await _userRepository.UpdateAsync(userId, user);
+
+        return DbResult > 0;
+    }
+}
