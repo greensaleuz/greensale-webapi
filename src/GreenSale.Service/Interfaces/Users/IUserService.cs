22f596c2dc4d9a0be1052d7c850c47d504459ecf
rustambekovich <muhammadqodir5050@gmail.com>
2023-08-15 10:47:40 +0500

    PERSISTENCE : Valodators User Update Validators

--- src/GreenSale.Service/Interfaces/Users/IUserService.cs
+++ src/GreenSale.Service/Interfaces/Users/IUserService.cs
@@ -1,13 +1,14 @@
 ﻿using GreenSale.Application.Utils;
 using GreenSale.DataAccess.ViewModels.Users;
 using GreenSale.Persistence.Dtos;
+using GreenSale.Persistence.Dtos.UserDtos;
 
 namespace GreenSale.Service.Interfaces.Users;
 
 public interface IUserService
 {
     public Task<bool> DeleteAsync(long userId);
-    public Task<bool> UpdateAsync(long userId, UserRegisterDto dto);
+    public Task<bool> UpdateAsync(long userId, UserUpdateDto dto);
     public Task<long> CountAsync();
     public Task<List<UserViewModel>> GetAllAsync(PaginationParams @params);
     public Task<UserViewModel> GetByIdAsync(long userId);
