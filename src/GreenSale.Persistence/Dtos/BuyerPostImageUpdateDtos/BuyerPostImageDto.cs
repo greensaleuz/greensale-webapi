7863a8eca8bb311a3353c9a2029ac841f887a5cd
rustambekovich <muhammadqodir5050@gmail.com>
2023-08-16 10:03:49 +0500

    Buyer Post Image Update Dto Creted

---
+++ src/GreenSale.Persistence/Dtos/BuyerPostImageUpdateDtos/BuyerPostImageDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace GreenSale.Persistence.Dtos.BuyerPostImageUpdateDtos;
+
+public class BuyerPostImageDto
+{
+    public long BuyerPostImageId { get; set; }
+    public long BuyerPostId { get; set; }
+    public IFormFile? ImagePath { get; set; }
+}
