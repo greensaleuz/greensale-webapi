98f5e143efdb2128c104b1810ca781b0bafce15a
rustambekovich <muhammadqodir5050@gmail.com>
2023-08-20 23:06:34 +0500

    Cors check amd programs

--- src/GreenSale.WebApi/Program.cs
+++ src/GreenSale.WebApi/Program.cs
@@ -10,8 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddMemoryCache();
 builder.Services.AddHttpContextAccessor();
+builder.Services.AddMemoryCache();
+
 builder.ConfigureJwtAuth();
 builder.ConfigureSwaggerAuth();
 builder.ConfigureCORSPolicy();
