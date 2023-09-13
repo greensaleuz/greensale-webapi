cc250de4f875972b31b5fea01e4b86b74ecc502b
Bakhtiyorovna <kralecabaxtiyorovna@gmail.com>
2023-09-13 17:25:57 +0500

    DATAACCESS : Writing An Interface To The Post Stars

---
+++ src/GreenSale.DataAccess/Interfaces/SellerPosts/ISellerPostStarRepository.cs
@@ -0,0 +1,8 @@
+﻿using GreenSale.Domain.Entites.SellerPosts;
+
+namespace GreenSale.DataAccess.Interfaces.SellerPosts;
+
+public interface ISellerPostStarRepository: IRepository<SellerPostStars, SellerPostStars>
+{
+    public Task<long> GetAllPostIdCountAsync(long id);
+}
