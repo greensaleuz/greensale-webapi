da0e8f65db868207dc27c7e40e8afee6e13d701f
Bakhtiyorovna <kralecabaxtiyorovna@gmail.com>
2023-09-13 18:36:52 +0500

    DATAACCESS : Writing An Repositories To The Post Stars

--- src/GreenSale.DataAccess/Interfaces/BuyerPosts/IBuyerPostStarRepository.cs
+++ src/GreenSale.DataAccess/Interfaces/BuyerPosts/IBuyerPostStarRepository.cs
@@ -6,4 +6,5 @@ namespace GreenSale.DataAccess.Interfaces.BuyerPosts;
 public interface IBuyerPostStarRepository : IRepository<BuyerPostStars,BuyerPostStars>
 {
     public Task<long> GetAllPostIdCountAsync(long id);
+    public Task<long> GetIdAsync(long userid, long postid);
 }
