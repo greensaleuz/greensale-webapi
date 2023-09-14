2db68404b423efe923b19c5636943bd110530326
Bakhtiyorovna <kralecabaxtiyorovna@gmail.com>
2023-09-15 02:03:22 +0500

    Service : Interface AND Sevice Write

--- src/GreenSale.Service/Interfaces/Storages/IStoragePostStarService.cs
+++ src/GreenSale.Service/Interfaces/Storages/IStoragePostStarService.cs
@@ -1,13 +1,14 @@
 ﻿using GreenSale.Application.Utils;
 using GreenSale.Domain.Entites.Storages;
 using GreenSale.Persistence.Dtos.SellerPostsDtos;
+using GreenSale.Persistence.Dtos.StoragDtos;
 
 namespace GreenSale.Service.Interfaces.Storages;
 
 public interface IStoragePostStarService
 {
-    public Task<int> CreateAsync(SellerPostStarCreateDto dto);
-    public Task<int> UpdateAsync(long Id, SellerPostStarUpdateDto dto);
+    public Task<int> CreateAsync(StorageStarCreateDto dto);
+    public Task<int> UpdateAsync(long PostId, StorageStarUpdateDto dto);
     public Task<long> CountAsync();
     public Task<List<StoragePostStars>> GetAllAsync(PaginationParams @params);
     public Task<StoragePostStars> GetByIdAsync(long Id);
