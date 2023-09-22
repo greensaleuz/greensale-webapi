f7f6fa514ee567253f57c217fd5eface70a2192d
rustambekovich <muhammadqodir5050@gmail.com>
2023-09-23 04:58:08 +0500

    change base connection

--- src/GreenSale.DataAccess/Repositories/BaseRepository.cs
+++ src/GreenSale.DataAccess/Repositories/BaseRepository.cs
@@ -11,8 +11,7 @@ public class BaseRepository
         Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
 
-        //string connection = "Host=dbaas-db-8432700-do-user-14588616-0.b.db.ondigitalocean.com; Port=25060; Database=greensale-server; User Id=doadmin; Password=AVNS_7Y14YLpKBS_teIyr_YW;";
-        string connection = "malika-databse-do-user-14588616-0.b.db.ondigitalocean.com; Port=25060; Database=greensale-server; User Id=doadmin; Password=AVNS_tMy1Hbz4D-xr3qLLEfK;";
+        
         //string connection = "Host = localhost; Database = greensale-db; Port = 5432; User Id = postgres; Password = 1234";
         this._connection = new NpgsqlConnection();
     }
