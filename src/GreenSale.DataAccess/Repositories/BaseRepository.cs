fe23a5fcaa99690fd58a0e6332a57094f2845842
SamandarYigitaliyev <programmist4467@gmail.com>
2023-09-22 20:30:44 +0500

    Connection removed

--- src/GreenSale.DataAccess/Repositories/BaseRepository.cs
+++ src/GreenSale.DataAccess/Repositories/BaseRepository.cs
@@ -10,8 +10,8 @@ public class BaseRepository
     {
         Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
 
-         string connection = "Host=dbaas-db-8432700-do-user-14588616-0.b.db.ondigitalocean.com; Port=25060; Database=greensale-server; User Id=doadmin; Password=AVNS_7Y14YLpKBS_teIyr_YW;";
+        //string connection = "Host=dbaas-db-8432700-do-user-14588616-0.b.db.ondigitalocean.com; Port=25060; Database=greensale-server; User Id=doadmin; Password=AVNS_7Y14YLpKBS_teIyr_YW;";
         //string connection = "Host = localhost; Database = greensale-db; Port = 5432; User Id = postgres; Password = 1234";
-        this._connection = new NpgsqlConnection(connection);
+        this._connection = new NpgsqlConnection();
     }
 }
